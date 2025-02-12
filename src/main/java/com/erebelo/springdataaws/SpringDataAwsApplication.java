fca8725b841f13bb38babd44c64cec6522c9499d
erebelo <eduldrebelo@gmail.com>
2025-02-12 14:21:45 -0300

    First commit

---
+++ src/main/java/com/erebelo/springdataaws/SpringDataAwsApplication.java
@@ -0,0 +1,13 @@
+package com.erebelo.springdataaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erebelo/springdataaws/SpringDataAwsApplicationTest.java
@@ -0,0 +1,13 @@
+package com.erebelo.springdataaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAwsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
