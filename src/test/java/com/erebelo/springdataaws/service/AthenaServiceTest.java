4668da94226399198af3e518f4a382655826fd0c
erebelo <eduldrebelo@gmail.com>
2025-02-14 23:05:31 -0300

    test2

--- src/test/java/com/erebelo/springdataaws/service/AthenaServiceTest.java
+++ src/test/java/com/erebelo/springdataaws/service/AthenaServiceTest.java
@@ -232,11 +232,6 @@ class AthenaServiceTest {
                 .isThrownBy(() -> service.getQueryResultsAsStrings(queryExecutionId))
                 .withMessage("Failed to get query results");
 
-        assertThatExceptionOfType(AthenaQueryException.class)
-                .isThrownBy(() -> service.getQueryResultsAsStrings(queryExecutionId))
-                .withMessage("Failed to get query results2");
-
-
         verify(athenaClient).getQueryResultsPaginator(any(GetQueryResultsRequest.class));
     }
 
