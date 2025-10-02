06a8aec2f9e15af93c1965edfb377932ad8fb085
erebelo <eduldrebelo@gmail.com>
2025-10-02 13:18:08 -0300

    Setting MongoDB connection settings

--- src/main/java/com/erebelo/springdataaws/hydration/repository/contract/ContractQueries.java
+++ src/main/java/com/erebelo/springdataaws/hydration/repository/contract/ContractQueries.java
@@ -1,20 +1,21 @@
 package com.erebelo.springdataaws.hydration.repository.contract;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PropertyPlaceholderHelper;
 
-import java.util.Map;
-
+@Getter
+@Setter
 @Component
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "athena.hydration")
 public class ContractQueries {
 
-    private final String database;
-    private final String advisorContract;
-    private final String firmContract;
+    private String databaseName;
+    private String advisorContract;
+    private String firmContract;
 
     private static final String ADVISOR_CONTRACT_QUERY_TEMPLATE = """
                SELECT ROW_NUMBER() OVER () AS recordId, *
@@ -31,19 +32,17 @@ public class ContractQueries {
     private final PropertyPlaceholderHelper placeholderHelper = new PropertyPlaceholderHelper("${", "}");
 
     private Map<String, String> buildHydrationTables(Long runNumber) {
-        return Map.of(
-                "db", database,
-                "advisor", advisorContract,
-                "firm", firmContract,
-                "runNumber", String.valueOf(runNumber)
-        );
+        return Map.of("db", databaseName, "advisor", advisorContract, "firm", firmContract, "runNumber",
+                String.valueOf(runNumber));
     }
 
     public String getAdvisorContractDataQuery(Long runNumber) {
-        return placeholderHelper.replacePlaceholders(ADVISOR_CONTRACT_QUERY_TEMPLATE, buildHydrationTables(runNumber)::get);
+        return placeholderHelper.replacePlaceholders(ADVISOR_CONTRACT_QUERY_TEMPLATE,
+                buildHydrationTables(runNumber)::get);
     }
 
     public String getFirmContractDataQuery(Long runNumber) {
-        return placeholderHelper.replacePlaceholders(FIRM_CONTRACT_QUERY_TEMPLATE, buildHydrationTables(runNumber)::get);
+        return placeholderHelper.replacePlaceholders(FIRM_CONTRACT_QUERY_TEMPLATE,
+                buildHydrationTables(runNumber)::get);
     }
 }
